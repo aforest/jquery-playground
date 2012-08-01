19ad0f9e9d4b015d68132de660eb84193cb9ba7b
Austin Forest <cforest@pattonboggs.com>
2012-08-01 11:20:22 -0600

    adding function

--- behavior.js
+++ behavior.js
@@ -2,14 +2,30 @@ function ExampleBehavior () {
   this.initialize();
 }
 
-ExampleBehavior.prototype = {
+ExampleBehavior.prototype = {}
   initialize: function () {
+    var self = this;
     this.sayHello();
-  },
-  sayHello: function () {
-    console.log("Here I am, come find me.");
-  }
+
+    $("#bands li").click(function() {
+  console.log("form was submitted");
+      self.removeArtist(this);
+    $("#add-band").submit (function() {
+      return false;
+    });
+},
+sayHello: function () {
+  console.log("Here I am, come find me.");
 };
+removeArtist: function (li) {
+  $(li).hide();
+}
+  };
+  addArtistFromForm: function () {
+    var artist = $("#add-band input [name='band']").val();
+    console.log("new artist is:" + artist);
+  }
+  };
 
 $(document).ready(function () {
   new ExampleBehavior();
